e66db0f20b7fec92c1135ece29e6fb53b85d0953
Morten Lied Johansen <mortenjo@ifi.uio.no>
2023-06-04 15:26:38 +0200

    No panic, only error, walking skeleton complete (?)

--- src/ingress.rs
+++ src/ingress.rs
@@ -1,8 +1,10 @@
 use kube::{Client, CustomResource};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use tokio::sync::mpsc::Receiver;
 use tokio_stream::wrappers::ReceiverStream;
 use tonic::{Code, Request, Response, Status};
+use tracing::warn;
 
 use crate::protogen::extension::{DefaultResponse, DocumentationRequest, DocumentationResponse, Response as ExtensionResponse, SyncRequest, ValidationRequest, ValidationResponse};
 use crate::protogen::extension::extension_server::Extension;
@@ -54,14 +56,15 @@ impl Extension for IngressHandler {
     type SyncStream = ReceiverStream<Result<ExtensionResponse, Status>>;
 
     async fn sync(&self, request: Request<SyncRequest>) -> Result<Response<Self::SyncStream>, Status> {
-        let request = request.into_inner();
-        todo!();
+        warn!("sync called, not implemented");
+        Err(Status::new(Code::Ok, "Sync not implemented"))
     }
 
     type DeleteStream = ReceiverStream<Result<ExtensionResponse, Status>>;
 
     async fn delete(&self, request: Request<SyncRequest>) -> Result<Response<Self::DeleteStream>, Status> {
-        todo!()
+        warn!("delete called, not implemented");
+        Err(Status::new(Code::Ok, "Delete not implemented"))
     }
 
     async fn default(&self, _request: Request<SyncRequest>) -> Result<Response<DefaultResponse>, Status> {
